d3e6bffadbb315e9973ea39219f0871d63d65d32
rmit3761929 <57607953+rmit3761929@users.noreply.github.com>
2022-01-04 20:26:03 +0700

    first commit

---
+++ app/src/main/java/com/example/assignment3/Home.java
@@ -0,0 +1,16 @@
+package com.example.assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_book);
+
+    }
+}
---
+++ app/src/main/java/com/example/assignment3/User.java
@@ -0,0 +1,6 @@
+package com.example.assignment3;
+
+public class User {
+    String name;
+    int id;
+}
